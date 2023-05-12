822f4644601a2e0779af19c59ed8b62995cc413b
veeRONy <99815306+veeRONy@users.noreply.github.com>
2023-05-12 18:02:25 +0500

    Изменение Repository

---
+++ Курсач/_Repository/IRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Курсач.Models
+{
+    public interface IRepository
+    {
+        void AddConf(ConfModel confModel);
+        void EditConf(ConfModel confModel);
+        void DeleteConf(int id);
+        IEnumerable<ConfModel> GetAllConfs();
+        IEnumerable<OrgModel> GetAllOrganizers();
+        IEnumerable<ConfModel> GetAllConfsForCB();
+        IEnumerable<ConfModel> GetByValueConfs(string value);
+
+        void AddOrg(OrgModel orgModel);
+        void EditOrg(OrgModel orgModel);
+        void DeleteOrg(int id);
+        IEnumerable<OrgModel> GetAllOrgs();
+        IEnumerable<OrgModel> GetByValueOrgs(string value);
+
+
+        void AddPart(ParticipantModel partModel);
+        void EditPart(ParticipantModel partModel);
+        void DeletePart(int id);
+        void AddReg(Conf_Part_Model confpartModel);
+        void DeleteReg(int part_id, int conf_id, string topic);
+        IEnumerable<Conf_Part_Model> GetAllConfPart();
+        IEnumerable<ParticipantModel> GetAllParts();
+        IEnumerable<ParticipantModel> GetByValueParts(string value);
+    }
+}
